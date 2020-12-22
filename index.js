5031873826d3b92ea167bc5a271a9d1a16433f14
Aluno manhã <alunomanha@senai.ti>
2020-12-22 12:30:15 -0300

    atualizando projeto

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var btn = document.querySelector("#btn");
 btn.addEventListener("click", function() {
     this.classList.toggle("active");
     const ativ = btn.getAttribute("class");
+    if (ativ == "active") {
+        body.style.background = "#333";
+        body.style.color = "#fff";
+        
+    } else {
+        body.style.background = "#fff";
+        body.style.color = "#333";
+    }
 
-}
+})
 
-if (ativ == "active") {
-    body.style.background = "#333";
-    body.style.color = "#fff";
-    
-} else {
-    body.style.background = "#fff";
-    body.style.color = "#333";
-}
